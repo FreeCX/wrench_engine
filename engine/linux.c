3f28d5dac2af0fe88056303b6f74d1fdc89e52ef
dr.FreeCX <dr.FreeCX@gmail.com>
2012-12-29 13:04:21 +0400

    add main function for linux window system

--- engine/linux.c
+++ engine/linux.c
@@ -2,8 +2,39 @@
 //    Programm:  Wrench Engine
 //        Type:  Source Code
 //      Module:  Window
-// Last update:  05/12/12
-// Description:  Оконная система (linux)
+// Last update:  29/12/12
+// Description:  Window system (linux)
 //
 
 #include "linux.h"
+
+int WE_Init(int argc, char **argv)
+{
+    /* insert code */
+}
+
+int WE_InitOpenGL(int mask)
+{
+    /* insert code */
+}
+
+int WE_CreateWindow(we_engine * engine)
+{
+    /* insert code */
+}
+
+int WE_Loop(void)
+{
+    /* insert code */
+}
+
+void WE_Kill(void)
+{
+    /* insert code */
+}
+
+void WE_SwapBuffers(void)
+{
+    /* insert code */
+}
+
--- engine/linux.h
+++ engine/linux.h
@@ -2,11 +2,27 @@
 //    Programm:  Wrench Engine
 //        Type:  Header
 //      Module:  Window
-// Last update:  05/12/12
-// Description:  Оконная система (linux)
+// Last update:  29/12/12
+// Description:  Window system (linux)
 //
 
 #ifndef __KERNEL_WINDOW_LINUX__
 #define __KERNEL_WINDOW_LINUX__
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include "structures.h"
+
+/* export function */
+
+int WE_Init(int argc, char **argv);
+int WE_InitOpenGL(int mask);
+int WE_CreateWindow(we_engine * engine);
+
+int WE_Loop(void);
+void WE_Kill(void);
+
+void WE_SwapBuffers(void);
 
 #endif
