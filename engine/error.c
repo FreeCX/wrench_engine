b5906506d06bdd36fa016993efcb97ade99929a9
dr.FreeCX <dr.FreeCX@gmail.com>
2012-12-29 13:00:46 +0400

    update error module

--- engine/error.c
+++ engine/error.c
@@ -2,8 +2,36 @@
 //    Programm:  Wrench Engine
 //        Type:  Source Code
 //      Module:  Error
-// Last update:  05/12/12
-// Description:  Система обработки ошибок
+// Last update:  29/12/12
+// Description:  Error handling system
 //
 
 #include "error.h"
+
+static int last_error = 0;
+static int str_error[] = "> ERROR: %s\n";
+
+static char *str_error_msg[] = {
+    "No error",
+    "Can't allocate memory"
+};
+
+void WE_SendError(int error)
+{
+    last_error = error;
+}
+
+int WE_GetError(void)
+{
+    return last_error;
+}
+
+char ** WE_GetErrorString(void)
+{
+    return str_error_msg[last_error];
+}
+
+void WE_PrintError(void)
+{
+    printf(str_error, str_error_msg[last_error]);
+}
--- engine/error.h
+++ engine/error.h
@@ -2,11 +2,27 @@
 //    Programm:  Wrench Engine
 //        Type:  Header
 //      Module:  Error
-// Last update:  05/12/12
-// Description:  Система обработки ошибок
+// Last update:  29/12/12
+// Description:  Error handling system
 //
 
 #ifndef __KERNEL_ERROR__
 #define __KERNEL_ERROR__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum {
+    WE_ERROR_NONE,
+    WE_ERROR_ALLOC_MEMORY
+};
+
+/* export function */
+
+void WE_SendError(int error);
+int WE_GetError(void);
+char ** WE_GetErrorString(void);
+void WE_PrintError(void);
 
 #endif
