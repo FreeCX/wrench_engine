869f7a4197bb6423088ee65560a4260a0e3017ef
dr.FreeCX <dr.FreeCX@gmail.com>
2012-12-29 13:04:40 +0400

    add main function for Windows window system

--- engine/windows.c
+++ engine/windows.c
@@ -2,8 +2,38 @@
 //    Programm:  Wrench Engine
 //        Type:  Source Code
 //      Module:  Window
-// Last update:  05/12/12
-// Description:  Оконная система (windows)
+// Last update:  29/12/12
+// Description:  Window system (windows)
 //
 
 #include "windows.h"
+
+int WE_Init(int argc, char **argv)
+{
+    /* insert code */
+}
+
+int WE_InitOpenGL(int mask)
+{
+    /* insert code */
+}
+
+int WE_CreateWindow(we_engine * engine)
+{
+    /* insert code */
+}
+
+int WE_Loop()
+{
+    /* insert code */
+}
+
+void WE_Kill(void)
+{
+    /* insert code */
+}
+
+void WE_SwapBuffers(void)
+{
+    /* insert code */
+}
--- engine/windows.h
+++ engine/windows.h
@@ -2,11 +2,28 @@
 //    Programm:  Wrench Engine
 //        Type:  Header
 //      Module:  Window
-// Last update:  05/12/12
-// Description:  Оконная система (windows)
+// Last update:  29/12/12
+// Description:  Window system (windows)
 //
 
 #ifndef __KERNEL_WINDOW_WINDOWS__
 #define __KERNEL_WINDOW_WINDOWS__
+
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+
+#include "structures.h"
+
+/* export function */
+
+int WE_Init(int argc, char **argv);
+int WE_InitOpenGL(int mask);
+int WE_CreateWindow(we_engine * engine);
+
+int WE_Loop(void);
+void WE_Kill(void);
+
+void WE_SwapBuffers(void);
 
 #endif
