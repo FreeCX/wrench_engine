687ae3ea19d8853e57a25cee7aa71adfc96fcf17
dr.FreeCX <dr.FreeCX@gmail.com>
2012-12-29 13:03:50 +0400

    adding main structures

--- engine/structures.h
+++ engine/structures.h
@@ -2,11 +2,26 @@
 //    Programm:  Wrench Engine
 //        Type:  Header
 //      Module:  Kernel
-// Last update:  05/12/12
-// Description:  Основные структуры для движка
+// Last update:  29/12/12
+// Description:  Basic structure of the system
 //
 
 #ifndef __KERNEL_STRUCTURES__
 #define __KERNEL_STRUCTURES__
+
+#define WE_FAILED			-1
+#define WE_SUCCESS			 1
+
+#ifdef __WIN32__
+    typedef unsigned __int64 uint;
+#endif
+
+typedef struct {
+	char *name;
+	int width;
+	int height;
+	int fullscreen;
+	int debug;
+} we_engine;
 
 #endif
