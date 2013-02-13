724985e194ead4d428b88c6c6b020dba192eb857
dr.FreeCX <dr.FreeCX@gmail.com>
2013-02-13 19:17:14 +0400

    date upd

--- engine/kernel.c
+++ engine/kernel.c
@@ -2,14 +2,14 @@
 //    Programm:  Wrench Engine
 //        Type:  Source Code
 //      Module:  Kernel
-// Last update:  11/02/13
+// Last update:  13/02/13
 // Description:  The core of the system
 //
 
 #include "kernel.h"
 
 static char *engine_name = "Wrench Engine";
-static char *engine_date = "11/02/13";
+static char *engine_date = "13/02/13";
 static int major_version   = 0;
 static int minor_version   = 1;
 static int release_version = 0;
