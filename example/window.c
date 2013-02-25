93969749f210655ae20864c0987c5754e043110e
dr.FreeCX <dr.FreeCX@gmail.com>
2013-02-25 19:34:43 +0400

    adding testing app

---
+++ example/window.c
@@ -0,0 +1,11 @@
+#include "../engine/kernel.h"
+
+int main( int argc, char *argv[] )
+{
+	weInit( argc, argv );
+	weInitWindow( 500, 500, 0 );
+	weInitOpenGL( 0 );
+	weCreateWindow( "Wrench Engine #1" );
+	weLoop();
+	return 0;
+}
