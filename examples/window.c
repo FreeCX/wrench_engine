96eef75e90b11df51e732cfa340164a1be711ce7
Голубев Алексей <dr.freecx@gmail.com>
2013-02-28 17:45:03 +0400

    code fix

--- examples/window.c
+++ examples/window.c
@@ -10,18 +10,18 @@ void resize( int width, int height )
 {
 	GLfloat param1, n = 2.0f;
 
-    param1 = (float)width/height;
-    glViewport( 0, 0, width, height );
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity();
+	param1 = (float)width/height;
+	glViewport( 0, 0, width, height );
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
 	if ( width <= height ) {
 		glOrtho( -n, n, -n/param1, n/param1, n, -n );
 	} else {
-		glOrtho( -n * param1, n * param1, -n, n, n, -n );
+	glOrtho( -n * param1, n * param1, -n, n, n, -n );
 	}
 	gluPerspective( 0.0f, param1, 0.0f, 10.0f );
-    glMatrixMode( GL_MODELVIEW);
-    glLoadIdentity();
+	glMatrixMode( GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void render( void )
@@ -33,15 +33,14 @@ void render( void )
 	glTranslatef( 0.0f, 0.0f, 1.0f );
 	glPointSize( 5.0f );
 	glBegin( GL_POINTS );
+	for ( n = 0.0f; n < M_PI; n += 0.01f ) {
 		glColor3f( 1.0f, 0.0f, 0.0f );
-		for ( n = 0.0f; n < M_PI; n += 0.01f ) {
-			glColor3f( 1.0f, 0.0f, 0.0f );
-			glVertex2f( 1.0f * sin( t + n ), 1.0f * cos( t + n ) );
-			glColor3f( 0.0f, 1.0f, 0.0f );
-			glVertex2f( 0.8f * sin( 1.0f - t + n ), 0.8f * cos( 1.0f - t + n ) );
-			glColor3f( 0.0f, 0.0f, 1.0f );
-			glVertex2f( 0.6f * sin( t + n + 1.0f ), 0.6f * cos( t + n + 1.0f ) );
-		}
+		glVertex2f( 1.0f * sin( t + n ), 1.0f * cos( t + n ) );
+		glColor3f( 0.0f, 1.0f, 0.0f );
+		glVertex2f( 0.8f * sin( 1.0f - t + n ), 0.8f * cos( 1.0f - t + n ) );
+		glColor3f( 0.0f, 0.0f, 1.0f );
+		glVertex2f( 0.6f * sin( t + n + 1.0f ), 0.6f * cos( t + n + 1.0f ) );
+	}
 	glEnd();
 	weSwapBuffers();
 	t += 0.05f;
