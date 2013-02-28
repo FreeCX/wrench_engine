dc28f46d7a5fdbdeaafbaec885d57cc03e981e39
Голубев Алексей <dr.freecx@gmail.com>
2013-02-28 10:12:31 +0400

    build 2

--- engine/kernel.c
+++ engine/kernel.c
@@ -2,18 +2,18 @@
 //    Programm:  Wrench Engine
 //        Type:  Source Code
 //      Module:  Kernel
-// Last update:  27/02/13
+// Last update:  28/02/13
 // Description:  The core of the system
 //
 
 #include "kernel.h"
 
 static char *engine_name = "Wrench Engine";
-static char *engine_date = "27/02/13";
+static char *engine_date = "28/02/13";
 static int major_version   = 0;
 static int minor_version   = 1;
 static int release_version = 0;
-static int build_version   = 1;
+static int build_version   = 2;
 int __DEBUG__ = 0;
 
 int weInit( const int argc, char **argv )
