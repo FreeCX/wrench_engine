a5840770dae0cc6cbdc8c67a6af764c2870f03ea
Голубев Алексей <dr.freecx@gmail.com>
2013-03-09 19:35:40 +0400

    experimental ui

--- examples/window.c
+++ examples/window.c
@@ -1,67 +1,41 @@
 #include "../engine/kernel.h"
-#include <math.h>
-#include <time.h>
+#include "../engine/gui.h"
 
-void get_time( int *hour, int *min, int *sec )
-{
-	time_t now;
-	struct tm * t_info;
-	
-	now = time( NULL );
-	t_info = localtime( &now );
-	*hour = t_info->tm_hour;
-	*min = t_info->tm_min;
-	*sec = t_info->tm_sec;
-}
+uiButton MyBut = { 10, 10, 100, 25, "The But!" };
+uiFont font = { 14, 14, "Terminus", 0 };
 
 void init( void )
 {
 	glClearColor( 0.3f, 0.3f, 0.4f, 1.0f );
-}
-
-void resize( int width, int height )
-{
-	GLfloat param1, n = 2.0f;
-
-	param1 = (float)width/height;
-	glViewport( 0, 0, width, height );
-	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity();
-	if ( width <= height ) {
-		glOrtho( -n, n, -n/param1, n/param1, n, -n );
-	} else {
-		glOrtho( -n * param1, n * param1, -n, n, n, -n );
-	}
-	gluPerspective( 0.0f, param1, 0.0f, 10.0f );
-	glMatrixMode( GL_MODELVIEW);
-	glLoadIdentity();
+	uiFontBuild( &font );
 }
 
 void render( void )
 {
-	static float t = 0.0f, n = 0.0f;
-	int h, m, s;
-
-	get_time( &h, &m, &s );
 	glClear( GL_COLOR_BUFFER_BIT );
 	glLoadIdentity();
-	glTranslatef( 0.0f, 0.0f, 1.0f );
-	glPointSize( 3.0f );
-	glBegin( GL_POINTS );
-	for ( n = -3.9f*M_PI; n < 14.1f*M_PI; n += 0.1f ) {
-		glColor3f( 1.0f, 0.0f, 0.0f );
-		glVertex2f( 0.6f * cosf( -( 6.0f * (s+n) * M_PI ) / 180.0f), 
-			0.6f * sinf( -( 6.0f * (s+n) * M_PI ) / 180.0f) );
-		glColor3f( 0.0f, 1.0f, 0.0f );
-		glVertex2f( 0.8f * cosf( -( 6.0f * (m+n) * M_PI ) / 180.0f), 
-			0.8f * sinf( -( 6.0f * (m+n) * M_PI ) / 180.0f) );
-		glColor3f( 0.0f, 0.0f, 1.0f );
-		glVertex2f( cosf( -( 15.0f * (h+n) * 0.4f * M_PI ) / 180.0f + 1.35f ), 
-			sinf( -( 15.0f * (h+n) * 0.4f * M_PI ) / 180.0f + 1.35f ) );
-	}
-	glEnd();
+	uiButtonDraw( &MyBut, &font );
 	weSwapBuffers();
-	t += 0.05f;
+}
+
+void resize( int width, int height )
+{
+	glViewport( 0, 0, width, height );
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	glOrtho( 0.0f, (float)width, (float)height, 0.0f, -1.0f, 1.0f );
+	gluPerspective( 0.0f, 0.0f, 0.0f, 100.0f );
+	glMatrixMode( GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void mouse( int state, int button, int x, int y )
+{
+	if ( state == WE_STATE_DOWN ) {
+		if ( button == WE_LEFT_BUTTON ) {
+			/* input some code */
+		}
+	}
 }
 
 int main( int argc, char *argv[] )
@@ -72,7 +46,9 @@ int main( int argc, char *argv[] )
 	weCreateWindow( "Wrench Engine #1" );
 	weRenderFunc( render );
 	weResizeFunc( resize );
+	weMouseFunc( mouse );
 	init();
 	weLoop();
+	uiFontKill();
 	return 0;
 }
